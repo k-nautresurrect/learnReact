eaeb11a1749a5e576eecc9d7eefeab9b7fae7edb
k-nautresurrect <kushignite@gmail.com>
2021-12-23 09:21:02 +0530

    Returned Div to root

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component{
     render () {
-        return <h1>Hello World!</h1>;
+        // we used bracket because javascript edits the return with ; if there is nothing
+        return (
+            <div>
+                <h1>The Division is returned to root</h1>
+                <button> Increment </button>
+            </div>
+        );
     }
 }
 
