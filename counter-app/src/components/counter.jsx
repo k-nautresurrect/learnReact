fb36baec263bbc9accdfdecbff892ce89ce9e69d
k-nautresurrect <kushignite@gmail.com>
2021-12-23 09:30:20 +0530

    Used ReactFragment

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 
 class Counter extends Component{
+
+    
     render () {
+        // return can only return one element as createReactElement takes only one element at a time
         // we used bracket because javascript edits the return with ; if there is nothing
+        // React.Fragment is used to return directly all the elements
         return (
-            <div>
+            <React.Fragment>
                 <h1>The Division is returned to root</h1>
                 <button> Increment </button>
-            </div>
+            </React.Fragment>
         );
     }
 }
