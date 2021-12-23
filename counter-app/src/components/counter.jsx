9a3117bf5383e949528346da727d54b817485dd4
k-nautresurrect <kushignite@gmail.com>
2021-12-23 09:43:59 +0530

    Added state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component{
 
-    
+    // state is a property of Component which can store the information that is dynamic
+    state = {
+        count: 2,
+    }
+
     render () {
         // return can only return one element as createReactElement takes only one element at a time
         // we used bracket because javascript edits the return with ; if there is nothing
@@ -10,9 +14,16 @@ class Counter extends Component{
         return (
             <React.Fragment>
                 <h1>The Division is returned to root</h1>
+                {/* {} <- are used to write javascript code in return */}
+                <span>{this.countShow()}</span>
                 <button> Increment </button>
             </React.Fragment>
         );
+    }
+
+    countShow() {
+        const {count} = this.state;
+        return <h1>{count}</h1>;
     }
 }
 
