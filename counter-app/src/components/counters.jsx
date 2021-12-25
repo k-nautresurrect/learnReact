b55113e595ae1c1204b811e301dac0eb80655575
k-nautresurrect <kushignite@gmail.com>
2021-12-25 08:47:34 +0530

    Successfully used useState hook

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Counter from "./counter";
 
 function Counters() {
+  const [counters, setCounters] = useState([
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 },
+  ]);
+
   return (
+    // way 1 of adding element in the component
     <div>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+      {counters.map((counter) => (
+        <Counter key={counter.id} />
+      ))}
     </div>
   );
 }
